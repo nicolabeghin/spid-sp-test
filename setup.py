6910a4673ffc57a083ea7e5a9a38ebb47aa7c81d
Nicola Beghin <nicolabeghin@gmail.com>
2025-07-15 23:34:03 +0200

    python requirements - hardcode lxml==5.4.0

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         'pysaml2>=6.5.1',
         'xmlschema==1.7.1',
         'requests>=2.25.1',
-        'lxml>=4.6.2',
+        'lxml==5.4.0',
         'Jinja2>=2.11.3',
         'spid_compliant_certificates>=0.5.3'
         # 'sslyze>=4.0.4', # todo
